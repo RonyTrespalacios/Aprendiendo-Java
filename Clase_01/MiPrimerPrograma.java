7f9c23535244b1fbe49d3d8d00c53a13baaee833
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2024-10-02 23:07:49 -0500

    add class 01 java example

---
+++ Clase_01/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+public class MiPrimerPrograma {
+    public static void main(String[] args) {
+        int edad;
+        edad = 22;
+        String saludo = "Hola, tengo ";
+        System.out.println(saludo+edad+" anios");
+        boolean happy = true;
+        char feliz = 'D';
+        char troste = 'c';
+        if(happy){
+            System.out.println("Estoy feliz! :"+feliz);
+        }else{
+            System.out.println("Estoy triste! :"+troste);
+        }
+        float pi = 3.141516f;
+        System.out.println(pi);
+    }
+}
